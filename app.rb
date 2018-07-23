8eaddf4bb2ec84245cf5e4493923e4a96e87befb
Josh Varney <joshcainvarney@gmail.com>
2018-07-22 22:57:08 -0400

    finally got rid of the decimal at the end when not needed

--- app.rb
+++ app.rb
@@ -49,9 +49,30 @@ post '/calculator' do
 	first_name = params[:first_name]
 	last_name = params[:last_name]
 	user_name = params[:user_name]
-	num1 = params[:num1]	
+	num1 = params[:num1]
+		if num1.split("").count > 1
+			num3 = num1.split("").last
+				if num3 == "."
+					num1 = num1.split("")[0..-2].join
+				end
+		else
+			num1
+		end		
+	  num1
+	params[:num1] = num1
 	operator = params[:operator]
 	num2 = params[:num2]
+		if num2.split("").count > 1
+			num4 = num2.split("").last
+				if num4 == "."
+					num2 = num2.split("")[0..-2].join
+				end
+		else
+			num2
+		end		
+	  num2
+	params[:num2] = num2
+	p params
 	session[:solution] = calculator(params[:num1], params[:operator], params[:num2])
 	erb :calculator_page, locals:{user_name: params[:user_name], first_name: params[:first_name], last_name: params[:last_name], num1: params[:num1], operator: params[:operator], num2: params[:num2], solution: session[:solution]}
 end
